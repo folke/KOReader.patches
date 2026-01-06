aff0f7e4bbc6cbacd1dcf9326444d383be522c26
Folke Lemaitre <folke.lemaitre@gmail.com>
2026-01-06 09:37:03 +0100

    fix: use 4 spaces for tab

--- 2-new-collections-star.lua
+++ 2-new-collections-star.lua
@@ -9,52 +9,52 @@ local userpatch = require("userpatch")
 local Screen = require("device").screen
 
 local function patchCoverBrowserCollectionIndicator(plugin)
-  -- Grab Cover Grid mode and the individual Cover Grid items
-  local MosaicMenu = require("mosaicmenu")
-  local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
+    -- Grab Cover Grid mode and the individual Cover Grid items
+    local MosaicMenu = require("mosaicmenu")
+    local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
 
-  -- Store original MosaicMenuItem paintTo method
-  local orig_MosaicMenuItem_paint = MosaicMenuItem.paintTo
+    -- Store original MosaicMenuItem paintTo method
+    local orig_MosaicMenuItem_paint = MosaicMenuItem.paintTo
 
-  -- Override paintTo method to add collection indicator
-  function MosaicMenuItem:paintTo(bb, x, y)
-    -- Call the original paintTo method to draw the cover normally
-    orig_MosaicMenuItem_paint(self, bb, x, y)
+    -- Override paintTo method to add collection indicator
+    function MosaicMenuItem:paintTo(bb, x, y)
+        -- Call the original paintTo method to draw the cover normally
+        orig_MosaicMenuItem_paint(self, bb, x, y)
 
-    -- Get the cover image widget (target) and dimensions
-    local target = self[1][1][1]
-    if not target or not target.dimen then
-      return
+        -- Get the cover image widget (target) and dimensions
+        local target = self[1][1][1]
+        if not target or not target.dimen then
+            return
+        end
+
+        -- Add collection indicator for books in collections
+        if self.menu and self.menu.name ~= "collections" and ReadCollection:isFileInCollections(self.filepath) then
+            local left_margin = Screen:scaleBySize(7)
+            local radius = Screen:scaleBySize(10)
+            local top_margin = Screen:scaleBySize(7)
+
+            -- Use target.dimen.x and target.dimen.y which are relative to the cell
+            local center_x = target.dimen.x + left_margin + radius
+            local center_y = target.dimen.y + top_margin + radius
+
+            -- draw filled black circle
+            bb:paintCircle(center_x, center_y, radius, Blitbuffer.COLOR_BLACK)
+
+            -- Create smaller star (half the size)
+            local collection_mark = IconWidget:new({
+                icon = "star.white",
+                width = Screen:scaleBySize(25), -- Half the size of corner_mark_size
+                height = Screen:scaleBySize(25),
+                alpha = true,
+            })
+
+            -- position icon centered inside circle
+            local icon_x = center_x - math.floor(collection_mark.width / 2)
+            local icon_y = center_y - math.floor(collection_mark.height / 2)
+
+            collection_mark:paintTo(bb, icon_x, icon_y)
+        end
     end
-
-    -- Add collection indicator for books in collections
-    if self.menu and self.menu.name ~= "collections" and ReadCollection:isFileInCollections(self.filepath) then
-      local left_margin = Screen:scaleBySize(7)
-      local radius = Screen:scaleBySize(10)
-      local top_margin = Screen:scaleBySize(7)
-
-      -- Use target.dimen.x and target.dimen.y which are relative to the cell
-      local center_x = target.dimen.x + left_margin + radius
-      local center_y = target.dimen.y + top_margin + radius
-
-      -- draw filled black circle
-      bb:paintCircle(center_x, center_y, radius, Blitbuffer.COLOR_BLACK)
-
-      -- Create smaller star (half the size)
-      local collection_mark = IconWidget:new({
-        icon = "star.white",
-        width = Screen:scaleBySize(25), -- Half the size of corner_mark_size
-        height = Screen:scaleBySize(25),
-        alpha = true,
-      })
-
-      -- position icon centered inside circle
-      local icon_x = center_x - math.floor(collection_mark.width / 2)
-      local icon_y = center_y - math.floor(collection_mark.height / 2)
-
-      collection_mark:paintTo(bb, icon_x, icon_y)
-    end
-  end
 end
 
 userpatch.registerPatchPluginFunc("coverbrowser", patchCoverBrowserCollectionIndicator)
--- 2-series-badge-numbered.lua
+++ 2-series-badge-numbered.lua
@@ -25,140 +25,140 @@ local series_badge_cache = {}
 
 -- Function to get or create cached badge
 local function get_series_badge(series_index)
-  if series_badge_cache[series_index] then
+    if series_badge_cache[series_index] then
+        return series_badge_cache[series_index]
+    end
+
+    local series_text = TextWidget:new({
+        text = "#" .. series_index,
+        face = Font:getFace("cfont", font_size),
+        bold = true,
+        fgcolor = text_color,
+    })
+
+    local series_badge = FrameContainer:new({
+        linesize = Screen:scaleBySize(2),
+        radius = Screen:scaleBySize(border_corner_radius),
+        color = border_color,
+        bordersize = border_thickness,
+        background = background_color,
+        padding = Screen:scaleBySize(2),
+        margin = 0,
+        series_text,
+    })
+
+    -- Caching the badge
+    series_badge_cache[series_index] = {
+        badge = series_badge,
+        text = series_text,
+        width = series_text:getSize().w,
+        height = series_text:getSize().h,
+    }
+
     return series_badge_cache[series_index]
-  end
-
-  local series_text = TextWidget:new({
-    text = "#" .. series_index,
-    face = Font:getFace("cfont", font_size),
-    bold = true,
-    fgcolor = text_color,
-  })
-
-  local series_badge = FrameContainer:new({
-    linesize = Screen:scaleBySize(2),
-    radius = Screen:scaleBySize(border_corner_radius),
-    color = border_color,
-    bordersize = border_thickness,
-    background = background_color,
-    padding = Screen:scaleBySize(2),
-    margin = 0,
-    series_text,
-  })
-
-  -- Caching the badge
-  series_badge_cache[series_index] = {
-    badge = series_badge,
-    text = series_text,
-    width = series_text:getSize().w,
-    height = series_text:getSize().h,
-  }
-
-  return series_badge_cache[series_index]
 end
 
 local function patchAddSeriesIndicator(plugin)
-  local MosaicMenu = require("mosaicmenu")
-  local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
+    local MosaicMenu = require("mosaicmenu")
+    local MosaicMenuItem = userpatch.getUpValue(MosaicMenu._updateItemsBuildUI, "MosaicMenuItem")
 
-  if not MosaicMenuItem then
-    return
-  end
-
-  -- Store original methods
-  local orig_MosaicMenuItem_init = MosaicMenuItem.init
-  local orig_MosaicMenuItem_paint = MosaicMenuItem.paintTo
-
-  -- Override init to compute series info once
-  function MosaicMenuItem:init()
-    orig_MosaicMenuItem_init(self)
-
-    -- Only compute series info for books, not directories
-    if not self.is_directory and not self.file_deleted then
-      -- Get book info once during initialization
-      local bookinfo = require("bookinfomanager"):getBookInfo(self.filepath, self.do_cover_image)
-      if bookinfo and bookinfo.series and bookinfo.series_index then
-        self.series_index = bookinfo.series_index
-        self.has_series = true
-
-        -- Pre-calculate badge dimensions
-        local cached = get_series_badge(self.series_index)
-        self.series_badge = cached.badge
-        self.series_badge_width = cached.width
-        self.series_badge_height = cached.height
-      end
-    end
-  end
-
-  function MosaicMenuItem:paintTo(bb, x, y)
-    -- Call original paintTo
-    orig_MosaicMenuItem_paint(self, bb, x, y)
-
-    -- Only draw if we have series info
-    if self.has_series and self.series_badge then
-      local target = self[1][1][1]
-      if not target or not target.dimen then
+    if not MosaicMenuItem then
         return
-      end
-
-      -- Calculate position once
-      local d_w = math.ceil(target.dimen.w / 5)
-      local d_h = math.ceil(target.dimen.h / 10)
-
-      local ix
-
-      if BD.mirroredUILayout() then
-        ix = -math.floor(d_w) -- Half outside on left side
-        if not self.overflow_checked then
-          local x_overflow_left = x - target.dimen.x + ix
-          if x_overflow_left > 0 then
-            self.refresh_dimen = self[1].dimen:copy()
-            self.refresh_dimen.x = self.refresh_dimen.x - x_overflow_left
-            self.refresh_dimen.w = self.refresh_dimen.w + x_overflow_left
-          end
-          self.overflow_checked = true
-        end
-      else
-        ix = target.dimen.w - math.floor(d_w) -- Half outside on right side
-        if not self.overflow_checked then
-          local x_overflow_right = target.dimen.x + ix + d_w - x - self.dimen.w
-          if x_overflow_right > 0 then
-            self.refresh_dimen = self[1].dimen:copy()
-            self.refresh_dimen.w = self.refresh_dimen.w + x_overflow_right
-          end
-          self.overflow_checked = true
-        end
-      end
-
-      -- Move down on y axis
-      iy = 0
-
-      -- Calculate badge position (relative to target)
-      local badge_x = target.dimen.x + ix + (d_w - self.series_badge_width) / 2
-      local badge_y = target.dimen.y + iy + (d_h - self.series_badge_height) / 2
-
-      -- Paint the cached badge
-      self.series_badge:paintTo(bb, badge_x, badge_y)
     end
-  end
 
-  -- Clean up cache when item is destroyed
-  local orig_MosaicMenuItem_free = MosaicMenuItem.free
-  if orig_MosaicMenuItem_free then
-    function MosaicMenuItem:free()
-      -- Clear series-related data
-      self.has_series = nil
-      self.series_index = nil
-      self.series_badge = nil
-      self.series_badge_width = nil
-      self.series_badge_height = nil
-      self.overflow_checked = nil
+    -- Store original methods
+    local orig_MosaicMenuItem_init = MosaicMenuItem.init
+    local orig_MosaicMenuItem_paint = MosaicMenuItem.paintTo
 
-      -- Call original free
-      orig_MosaicMenuItem_free(self)
+    -- Override init to compute series info once
+    function MosaicMenuItem:init()
+        orig_MosaicMenuItem_init(self)
+
+        -- Only compute series info for books, not directories
+        if not self.is_directory and not self.file_deleted then
+            -- Get book info once during initialization
+            local bookinfo = require("bookinfomanager"):getBookInfo(self.filepath, self.do_cover_image)
+            if bookinfo and bookinfo.series and bookinfo.series_index then
+                self.series_index = bookinfo.series_index
+                self.has_series = true
+
+                -- Pre-calculate badge dimensions
+                local cached = get_series_badge(self.series_index)
+                self.series_badge = cached.badge
+                self.series_badge_width = cached.width
+                self.series_badge_height = cached.height
+            end
+        end
     end
-  end
+
+    function MosaicMenuItem:paintTo(bb, x, y)
+        -- Call original paintTo
+        orig_MosaicMenuItem_paint(self, bb, x, y)
+
+        -- Only draw if we have series info
+        if self.has_series and self.series_badge then
+            local target = self[1][1][1]
+            if not target or not target.dimen then
+                return
+            end
+
+            -- Calculate position once
+            local d_w = math.ceil(target.dimen.w / 5)
+            local d_h = math.ceil(target.dimen.h / 10)
+
+            local ix
+
+            if BD.mirroredUILayout() then
+                ix = -math.floor(d_w) -- Half outside on left side
+                if not self.overflow_checked then
+                    local x_overflow_left = x - target.dimen.x + ix
+                    if x_overflow_left > 0 then
+                        self.refresh_dimen = self[1].dimen:copy()
+                        self.refresh_dimen.x = self.refresh_dimen.x - x_overflow_left
+                        self.refresh_dimen.w = self.refresh_dimen.w + x_overflow_left
+                    end
+                    self.overflow_checked = true
+                end
+            else
+                ix = target.dimen.w - math.floor(d_w) -- Half outside on right side
+                if not self.overflow_checked then
+                    local x_overflow_right = target.dimen.x + ix + d_w - x - self.dimen.w
+                    if x_overflow_right > 0 then
+                        self.refresh_dimen = self[1].dimen:copy()
+                        self.refresh_dimen.w = self.refresh_dimen.w + x_overflow_right
+                    end
+                    self.overflow_checked = true
+                end
+            end
+
+            -- Move down on y axis
+            iy = 0
+
+            -- Calculate badge position (relative to target)
+            local badge_x = target.dimen.x + ix + (d_w - self.series_badge_width) / 2
+            local badge_y = target.dimen.y + iy + (d_h - self.series_badge_height) / 2
+
+            -- Paint the cached badge
+            self.series_badge:paintTo(bb, badge_x, badge_y)
+        end
+    end
+
+    -- Clean up cache when item is destroyed
+    local orig_MosaicMenuItem_free = MosaicMenuItem.free
+    if orig_MosaicMenuItem_free then
+        function MosaicMenuItem:free()
+            -- Clear series-related data
+            self.has_series = nil
+            self.series_index = nil
+            self.series_badge = nil
+            self.series_badge_width = nil
+            self.series_badge_height = nil
+            self.overflow_checked = nil
+
+            -- Call original free
+            orig_MosaicMenuItem_free(self)
+        end
+    end
 end
 
 userpatch.registerPatchPluginFunc("coverbrowser", patchAddSeriesIndicator)
